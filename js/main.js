0e25a836580c0b16eed5f046a5fe03bcbae2c0cf
Cheyenne Son <cheyenne.son@gmail.com>
2020-02-10 09:38:51 -0700

    some changes

--- js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $( ".shoeclick" ).click(function() {
-    $('.order-details').text("Thank you for ordering " + $(this).data('id') + "!");
-    $('.order-link').click();
-  });
-});
