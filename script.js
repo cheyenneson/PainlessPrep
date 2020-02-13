69923a980213e46e45bb16772748a37556ad39b2
EC2 Default User <ec2-user@ip-172-31-28-165.us-west-2.compute.internal>
2020-02-13 19:44:17 +0000

    add vue component

---
+++ script.js
@@ -0,0 +1,28 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        ingredient: {
+            item: '',
+            quantity: '',
+            preferred_brand: ''
+        },
+        ingredients: {},
+        addedItem: '',
+        addedQuantity: '',
+        addedBrand: ''
+    },
+    methods: {
+        addItem() {
+            this.ingredients.push({
+                item: this.addedItem,
+                quantity: this.addedQuantity,
+                preferred_brand: this.addedBrand
+            });
+            this.addedItem = '';
+            this.addedQuantity = '';
+            this.addedBrand = '';
+        },
+    },
+    computed: {
+    },
+});
