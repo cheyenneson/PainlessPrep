d51a7b1c62814b80f000990afdab3a3911991146
cheyenneson <cheyenne.son@gmail.com>
2020-02-29 21:20:21 +0000

    some styling

--- script.js
+++ script.js
@@ -33,3 +33,14 @@ let app = new Vue({
     },
     computed: {},
 });
+
+function myFunction() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
